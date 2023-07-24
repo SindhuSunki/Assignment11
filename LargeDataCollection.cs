16f18642bc1336fbf5843f97544b9a26677905b2
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-07-24 17:10:46 +0530

    first commit

---
+++ LargeDataCollection.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment12
+{
+    internal class LargeDataCollection : IDisposable
+    {
+        private List<string> list = null;
+        bool disposed;
+        public int Count;
+   
+
+        public LargeDataCollection(List<string> list)
+        {
+            this.list = list;
+            
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing)
+            {
+                if (disposing)
+                {
+                    Count = 0;
+                }
+
+            }
+             Console.WriteLine("Data has been disposed");
+             list = null;
+             disposed = true;
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        public void Push(string element)
+        {
+            list.Add(element);
+
+        }
+      public void Read()
+        {
+            foreach(string i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void Remove(string data)
+        {
+            list.Remove(data);
+        }
+        
+        public void GetItem(string data)
+        {
+            if (list.Contains(data))
+            {
+                Console.WriteLine($"{data} is available in List");
+            }
+            else
+            {
+                Console.WriteLine($"{data} is not available in List");
+            }
+
+        }
+       
+         public void GetFileDetails()
+         {
+             
+         }
+        ~LargeDataCollection()
+        {
+            Dispose(false);
+        }
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            List<string> list = new List<string>();
+
+            using (LargeDataCollection data = new LargeDataCollection(list))
+            {
+                Console.WriteLine(" Enter Total numbers Count");
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter numbers to Add");
+               for(int i=0;i<num;i++)
+
+                {
+                    
+                    string add = Console.ReadLine();
+                    data.Push(add);                    
+                }
+                               
+                Console.WriteLine("updated list is :");
+                data.Read();
+                Console.Write("\n");
+                Console.WriteLine("Enter number to Remove");
+                string Remove = Console.ReadLine();
+                data.Remove(Remove);
+              
+                Console.WriteLine("updated list is :");
+                data.Read();
+                Console.Write("\n");
+               // Console.WriteLine("After removing number updated list is"+ data);
+
+            };
+            //Console.WriteLine($"After removing number updated list is :{list}");
+           
+            Console.ReadKey();
+        }
+    }
+}
